7d0b78f2f181339a1108cb9d397f6198eac81e86
Morpheus-mana <sebastien.beser@live.fr>
2025-04-06 18:09:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask, request
-import json
-
-app = Flask(__name__)
-
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def recevoir():
-    contenu = request.get_json()
-    with open("dernier_message.json", "w") as f:
-        json.dump(contenu, f)
-    return {"status": "ok"}, 200
+    if request.method == "POST":
+        contenu = request.get_json()
+        with open("dernier_message.json", "w") as f:
+            json.dump(contenu, f)
+        return {"status": "ok"}, 200
+    return "⚠️ Tu dois utiliser POST avec un JSON", 200
