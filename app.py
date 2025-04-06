db720629c6994e8cd47b9feecb291c097e8bb114
Morpheus-mana <sebastien.beser@live.fr>
2025-04-06 18:42:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
 from flask import Flask, request
-import json
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def recevoir():
-    if request.method == "POST":
-        try:
-            contenu = request.get_json()
-            with open("dernier_message.json", "w") as f:
-                json.dump(contenu, f)
-            return {"status": "ok"}, 200
-        except:
-            return {"status": "erreur"}, 400
-    else:
-        return "<h1>✅ API en ligne</h1><p>Utilise POST avec du JSON pour envoyer des données.</p>", 200
-
-@app.route("/get-message", methods=["GET"])
-def get_message():
-    try:
-        with open("dernier_message.json", "r") as f:
-            contenu = json.load(f)
-        return contenu, 200
-    except:
-        return {}, 200
-
+    print(request.json)
+    return {"status": "ok"}, 200
